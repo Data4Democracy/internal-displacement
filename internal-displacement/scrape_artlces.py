770443985a2faa2e3dcef10216395d6e14649003
George Richardson <g.raymond.richardson@gmail.com>
2017-02-01 18:40:34 -0800

    create skeleton structure and placeholder functions

--- internal-displacement/scrape_artlces.py
+++ internal-displacement/scrape_artlces.py
@@ -4,8 +4,15 @@ from urllib import request
 import numpy as np
 
 
-class UrlList(object):
-    '''A wrapper for lists of urls in various formats.
+class Scraper(object):
+    '''Scraper that accepts a url (or urls) and returns an instance of Article
+    Parameters
+    ----------
+    urls: a url to be scraped
+
+    Returns
+    -------
+    article: instance of Article containing body text and 
     '''
     def __init__(self, urls):
         if isinstance(urls, list):
@@ -14,6 +21,8 @@ class UrlList(object):
             self.urls = list(urls)
         if isinstance(urls, str):
             self.urls = [urls]
+
+    # Main Functions #
 
     def sample_urls(self, size=0.25, random=True):
         '''Return a subsample of urls
@@ -54,6 +63,33 @@ class UrlList(object):
         else:
             return self.urls[:sample_size]
 
+    def get_content():
+        '''Returns the main text body from the url
+        '''
+        pass
+
+    def get_title():
+        '''Returns the article title from the url
+        '''
+        pass
+
+    def get_meta():
+        '''Returns date published
+        '''
+        pass
+
+    def tag_type():
+        '''Returns type of content (article/video/image/pdf)
+        '''
+        pass
+
+    def export_article():
+        '''Returns instance of article with content and all metadata
+        '''
+        pass
+
+    # Helper Functions #
+
     def remove_newline(text):
         ''' Removes new line and &nbsp characters.
         '''
@@ -69,3 +105,64 @@ class UrlList(object):
         _extracted = [s.extract() for s in soup(
             ['script', 'link', 'style', 'id', 'class', 'li', 'head', 'a'])]
         text = remove_newline(soup.get_text())
+
+class Article(object):
+    '''Contains article text, metadata, extracted information and tag
+    '''
+    def __init__(self):
+        pass
+
+    def tag():
+        '''Use interpreter to tag article
+        '''
+        pass
+
+    def parse():
+        '''Use interpreter to parse article
+        '''
+        pass
+
+    def export():
+        '''Save article to external file
+        '''
+        pass
+
+
+class Interpreter(oject):
+    '''The interpreter can use a pre-trained model or be trained to tag 
+    articles and extract useful information from them.
+    '''
+    def __init__(self):
+        pass
+
+    def train_tagger():
+        '''Train interpreter to tag articles as "distaster/violence/other"
+        '''
+        pass
+
+    def train_parser():
+        '''Train interpreter to parse articles and extract numbers and reporting unit
+        '''
+        pass
+
+    def load_model():
+        '''Load previously trained interpreter model
+        '''
+        pass
+
+    def save_model():
+        '''Export and save interpreter
+        '''
+        pass
+
+    def tag():
+        '''Tag article as "distaster/violence/other"
+        '''
+        pass
+
+    def parse():
+        '''Parse article to extract numbers and reporting unit
+        '''
+        pass
+
+
