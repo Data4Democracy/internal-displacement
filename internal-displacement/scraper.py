0d24cdff0d18f540da145063c02877e68c406a9a
George Richardson <g.raymond.richardson@gmail.com>
2017-02-03 21:29:53 -0800

    sample urls now in pipeline

--- internal-displacement/scraper.py
+++ internal-displacement/scraper.py
@@ -31,46 +31,6 @@ class Scraper(object):
         if isinstance(urls, str):
             self.urls = [urls]
 
-    def sample_urls(self, size=0.25, random=True):
-        '''Return a subsample of urls
-        Parameters
-        ----------
-        size: float or int, default 0.25.
-            If float, should be between 0.0 and 1.0 and is
-            the size of the subsample of return. If int, represents
-            the absolute size of the sample to return.
-
-        random: boolean, default True
-            Whether or not to generate a random or direct subsample.
-
-        Returns
-        -------
-        urls_sample: subsample of urls as Pandas Series
-        '''
-        if isinstance(size, int) and size <= len(self.urls):
-            sample_size = size
-        elif isinstance(size, int) and size > len(self.urls):
-            raise ValueError("Sample size cannot be larger than the"
-                             " number of urls.")
-        elif isinstance(size, float) and size >= 0.0 and size <= 1.0:
-            sample_size = int(size * len(self.urls))
-        else:
-            raise ValueError("Invalid sample size."
-                             " Please specify required sample size as"
-                             " a float between 0.0 and 1.0 or as an integer.")
-
-        if isinstance(random, bool):
-            randomize = random
-        else:
-            raise ValueError("Invalid value for random."
-                             " Please specify True or False.")
-
-        if randomize:
-            return np.random.choice(self.urls, sample_size)
-        else:
-            return self.urls[:sample_size]
-
-
     def html_article(self, url):
         '''Downloads and extracts content plus metadata for html page
         Parameters
