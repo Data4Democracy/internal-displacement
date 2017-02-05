524c9a14153576bf210eec245540e8ec7a591a8a
jlln <james.allen.1248@gmail.com>
2017-02-05 12:56:18 +0800

    sql based pipeline

--- internal_displacement/article.py
+++ internal_displacement/article.py
@@ -1,5 +1,16 @@
+import json
+import os
+import hashlib
+import datetime
+import sqlite3
+
+def date_time_converter(dt):
+    if isinstance(dt,datetime.datetime):
+        return dt.__str__()
+
+
 class Article(object):
-    '''Contains article text, date, extracted information and tag
+    """Contains article text, date, extracted information and tag
      Parameters
         ----------
         content:                the text from the article
@@ -8,33 +19,60 @@ class Article(object):
         authors:                the authors
         domain:                 the domain
         content_type:           the type of content (text,image,video etc)
+        url:                    the url of the article
 
-    '''
+    """
 
-    def __init__(self, content, pub_date, title,content_type,authors,domain):
+    def __init__(self, content, pub_date, title, content_type,authors,domain,url):
         self.content = content
         self.publication_date = pub_date
         self.title = title
         self.authors = authors
         self.domain = domain
         self.content_type = content_type
+        self.url = url
 
     def tag(self, tag):
-        '''Use interpreter to tag article
-        '''
+        """Use interpreter to tag article
+        """
         self.tag = tag
 
     def add_label(self, label):
-        '''Store a label for training cases
-        '''
+        """Store a label for training cases
+        """
         self.label = label
 
-    def parse():
-        '''Use interpreter to parse article
-        '''
+    def parse(self):
+        """Use interpreter to parse article
+        """
         pass
 
-    def export():
-        '''Save article to external file
-        '''
-        pass
+    def export_to_json(self, storage_dir):
+        """Save article to external json file
+        :param storage_dir:     a directory in which to write the json file
+
+        returns: The filepath to the newly created json file.
+        """
+        filename = hashlib.md5((self.domain + self.title + ",".join(self.authors)).encode('utf-8')).hexdigest() + ".json"
+        filepath = os.path.join(storage_dir, filename)
+        with open(filepath, 'w') as fp:
+            json.dump(self.__dict__, fp, ensure_ascii=False,default = date_time_converter)
+        return filepath
+
+    def insert_into_sql_table(self,sql_cursor):
+
+        url = self.url
+        authors = ",".join(self.authors)
+        pub_date = date_time_converter(self.publication_date)
+        domain = self.domain
+        content = self.content
+        content_type = self.content_type
+        try:
+            sql_cursor.execute("INSERT INTO Articles VALUES (?,?,?,?,?,?)", (url,authors,pub_date,domain,content,content_type))
+        except sqlite3.IntegrityError:
+            print("URL{url} already exists in article table. Skipping.".format(self.url))
+        except Exception as e:
+            print("Exception: {}".format(e))
+
+
+
--- internal_displacement/scraper.py
+++ internal_displacement/scraper.py
@@ -1,8 +1,23 @@
 import newspaper
 import csv
-from internal-displacement import Article
+from internal_displacement.article import Article
+import datetime
+
+
+# Helper Functions #
+
+def remove_newline(text):
+    """
+    Removes new line and &nbsp characters.
+    """
+    text = text.replace('\n', ' ')
+    text = text.replace('\xa0', ' ')
+    return text
+
 class Scraper(object):
-    '''Scraper that accepts a url (or urls) and returns an instance of Article
+
+
+    """Scraper that accepts a url (or urls) and returns an instance of Article
     Parameters
     ----------
     urls: a url to be scraped
@@ -10,47 +25,87 @@ class Scraper(object):
     Returns
     -------
     article: instance of Article containing body text and 
-    '''
+    """
 
-        # Helper Functions #
-
-    def remove_newline(self,text):
-        ''' Removes new line and &nbsp characters.
-        '''
-        text = text.replace('\n', ' ')
-        text = text.replace('\xa0', ' ')
-        return text
 
 
     # Class Functions #
+    def __init__(self):
+        pass
 
-    def html_article(self, url):
-        '''Downloads and extracts content plus metadata for html page
+    def sample_urls(self, size=0.25, random=True):
+        """Return a subsample of urls
+        Parameters
+        ----------
+        size: float or int, default 0.25.
+            If float, should be between 0.0 and 1.0 and is
+            the size of the subsample of return. If int, represents
+            the absolute size of the sample to return.
+
+        random: boolean, default True
+            Whether or not to generate a random or direct subsample.
+
+        Returns
+        -------
+        urls_sample: subsample of urls as Pandas Series
+        """
+        if isinstance(size, int) and size <= len(self.urls):
+            sample_size = size
+        elif isinstance(size, int) and size > len(self.urls):
+            raise ValueError("Sample size cannot be larger than the"
+                             " number of urls.")
+        elif isinstance(size, float) and size >= 0.0 and size <= 1.0:
+            sample_size = int(size * len(self.urls))
+        else:
+            raise ValueError("Invalid sample size."
+                             " Please specify required sample size as"
+                             " a float between 0.0 and 1.0 or as an integer.")
+
+        if isinstance(random, bool):
+            randomize = random
+        else:
+            raise ValueError("Invalid value for random."
+                             " Please specify True or False.")
+
+        if randomize:
+            return np.random.choice(self.urls, sample_size)
+        else:
+            return self.urls[:sample_size]
+        
+    def html_report(self, url):
+        """Downloads and extracts content plus metadata for html page
         Parameters
         ----------
         url: url of page to be scraped 
 
         Returns
         -------
-        report: dictionary containing page content and metadata
-        '''
+        article: An object of class Article containing the content and metadata.
+        """
 
         a = newspaper.Article(url)
         a.download()
-        a.parse()
-        article_domain = a.source_url
-        article_title = a.title
-        article_authors = a.authors
-        article_pub_date = a.publish_date
-        article_text = self.remove_newlines(a.text)
-        # tag the type of article
-        ## currently default to text but should be able to determine img/video etc
-        article_type = 'text'
-        article = Article
-        return article
+        if a.is_downloaded:
+            a.parse()
+            article_domain = a.source_url
+            article_title = a.title
+            print(article_title)
+            article_authors = a.authors
+            article_pub_date = a.publish_date
+            article_text = remove_newline(a.text)
+            # tag the type of article
+            ## currently default to text but should be able to determine img/video etc
+            article_content_type = 'text'
+            article = Article(article_text, article_pub_date, article_title,
+                              article_content_type, article_authors, article_domain, url)
+            return article
+        else: ##Temporary fix to deal with https://github.com/codelucas/newspaper/issues/280
+            return Article("", datetime.datetime.now(), "retrieval_failed", "", "", "", "")
+
 
     def scrape(self, urls):
-        '''Scrapes content and metadata from all pages in a list
+        """
+        Scrapes content and metadata from all pages in a list
         ** Currently skips pdfs and only calls html_report
         Parameters
         ----------
@@ -59,24 +114,19 @@ class Scraper(object):
         Returns
         -------
         reports: list of dictionaries containing all reports
-        '''
+        """
         articles = []
         for url in urls:
             if url[-3:] == 'pdf':
                 continue
             else:
-                article = html_article(url)
+                article = self.html_report(url)
                 articles.append(article)
                 
         return articles
 
 
 
-    def __init__(self, urls):
-        if isinstance(urls, list):
-            self.urls = urls
-        if isinstance(urls, pd.Series):
-            self.urls = list(urls)
-        if isinstance(urls, str):
-            self.urls = [urls]
+
+
 
