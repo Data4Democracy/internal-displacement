1a02a5a068596bf1b64d050836469439ff7ecacc
Simon Bedford <simon.bedford@gmail.com>
2017-02-08 16:09:21 -0600

    function for identifying the country or countries referenced

--- internal_displacement/filter.py
+++ internal_displacement/filter.py
@@ -1,9 +1,21 @@
 from langdetect import detect
+import pycountry
+import spacy
+
 
 class Filter(object):
     '''Filter that accepts an article and identifies the 
     language and relevance of the article
     '''
+
+    # Helper methods
+
+    def country_code(country_name):
+        try:
+            country_code = pycountry.countries.get(name=s).alpha_2
+            return country_code
+        except KeyError:
+            return False
 
     def __init__(self):
         pass
@@ -19,3 +31,23 @@ class Filter(object):
         upon its content.
         '''
         pass
+
+    def extract_countries(self, article):
+        '''Extract the ISO codes for the countries mentioned
+        in the article.
+        '''
+        nlp = spacy.load('en')
+        doc = nlp(u"{}".format(s))
+        possible_entities = []
+        for ent in doc.ents:
+            if ent.label_ == 'GPE':
+                possible_entities.append(ent.text)
+        possible_entities = set(possible_entities)
+
+        countries = []
+        for c in possible_entities:
+            code = country_code(c)
+            if code:
+                countries.append(code)
+
+        return countries
