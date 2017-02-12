ed2366dd9d76ab86d1bab46bb98a4073d843ffb4
Simon Bedford <simon.bedford@gmail.com>
2017-02-12 13:41:09 -0600

    Further enhancements to the country extract function to take into account cities, provinces, states etc.

--- internal_displacement/interpreter.py
+++ internal_displacement/interpreter.py
@@ -1,26 +1,71 @@
 from langdetect import detect
 import pycountry
 import spacy
+import json
+import re
+import os
 
 # Helper methods
 
 
-def country_code(country_name):
-    '''Find the ISO-3166 alpha_2 country code 
-    for a given country name
+def strip_words(place_name):
+    '''Strip out common words that often appear in extracted entities
     '''
-    if 'the' in country_name:
-        country_name = country_name.replace("the", "").strip()
-    try:
-        code = pycountry.countries.get(name=country_name).alpha_2
-        return code
-    except KeyError:
-        pass
-    try:
-        code = pycountry.countries.get(official_name=country_name).alpha_2
-        return code
-    except KeyError:
-        return None
+    place_name = place_name.lower()
+    words_to_replace = {"the": "", "province": "",
+                        "county": "", "district": "", "city": ""}
+    rep = dict((re.escape(k), v) for k, v in words_to_replace.items())
+    pattern = re.compile("|".join(rep.keys()))
+    place_name = pattern.sub(lambda m: rep[re.escape(m.group(0))], place_name)
+    return place_name.strip().title()
+
+
+def common_names(place_name):
+    '''Convert countries or places with commonly used names
+    to their official names
+    '''
+    return{
+        'Syria': 'Syrian Arab Republic',
+        'Bosnia': 'Bosnia and Herzegovina'
+    }.get(place_name, place_name)
+
+
+def province_country_code(place_name):
+    '''Try and extract the country code by looking
+    at country subdivisions i.e. States, Provinces etc.
+    return the country code if found
+    '''
+    subdivisions = (s for s in list(pycountry.subdivisions))
+    for sub_division in subdivisions:
+        if sub_division.name == place_name:
+            return sub_division.country_code
+            break
+
+
+def match_country_name(place_name):
+    '''Try and match the country name directly
+    return the country code if found
+    '''
+    countries = (c for c in list(pycountry.countries))
+    for country in countries:  # Loop through all countries
+        if country.name == place_name:  # Look directly at country name
+            return country.alpha_2
+            break
+        # In some cases the country also has a common name
+        elif hasattr(country, 'common_name') and country.common_name == place_name:
+            return country.alpha_2
+            break
+        # In some cases the country also has an official name
+        elif hasattr(country, 'official_name') and country.official_name == place_name:
+            return country.alpha_2
+            break
+        # In some cases the country name has the form Congo, The Democratic Republic of the
+        # which may be hard to match directly
+        elif re.match(r'\D+,\s{1}', country.name):
+            common = re.search(r'(\D+),\s{1}', country.name).groups()[0]
+            if common in place_name:
+                return country.alpha_2
+                break
 
 
 class Interpreter(object):
@@ -28,8 +73,10 @@ class Interpreter(object):
     from articles.
     '''
 
-    def __init__(self):
+    def __init__(self, data_path='../data'):
         self.nlp = spacy.load('en')
+        with open(os.path.join(data_path, 'cities_to_countries.json'), "r") as f:
+            self.cities_to_countries = json.load(f)
 
     def check_language(self, article):
         '''Identify the language of the article content
@@ -44,23 +91,39 @@ class Interpreter(object):
         '''
         pass
 
+    def country_code(self, place_name):
+        '''Find the ISO-3166 alpha_2 country code 
+        for a given country name
+        '''
+        place_name = common_names(place_name)
+        country_code_from_name = match_country_name(place_name)
+        if country_code_from_name:
+            return country_code_from_name
+        # Try getting the country code using a province name
+        country_from_province = province_country_code(place_name)
+        if country_from_province:
+            return country_from_province
+        # Try getting the country code using a city name
+        return self.cities_to_countries.get(place_name, None)
+
     def extract_countries(self, article):
         '''Extract the ISO codes for the countries mentioned
         in the article, and return an array containing all
         mentioned countries
         '''
-        doc = self.nlp(u"{}".format(article.content))
-        possible_entities = []
+        text = " ".join([article.title, article.content])
+        doc = self.nlp(u"{}".format(text))
+        possible_entities = set()
         for ent in doc.ents:
-            if ent.label_ == 'GPE':
-                possible_entities.append(ent.text)
+            if ent.label_ in ('GPE', 'LOC'):
+                possible_entities.add(strip_words(ent.text))
         possible_entities = set(possible_entities)
 
-        countries = []
+        countries = set()
         if len(possible_entities) > 0:
             for c in possible_entities:
-                code = country_code(c)
+                code = self.country_code(c)
                 if code:
-                    countries.append(code)
+                    countries.add(code)
 
-        return countries
+        return list(countries)
--- internal_displacement/tests/test_Interpreter.py
+++ internal_displacement/tests/test_Interpreter.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from internal_displacement.interpreter import Interpreter
+from internal_displacement.interpreter import *
 from internal_displacement.article import Article
 from langdetect import detect
 import pycountry
@@ -10,7 +10,7 @@ import datetime
 class TestInterpreter(TestCase):
 
     def setUp(self):
-        self.pipeline = Interpreter()
+        self.pipeline = Interpreter(data_path='data')
         self.date = datetime.datetime.now()
 
     def tearDown(self):
@@ -24,15 +24,25 @@ class TestInterpreter(TestCase):
         article_language = test_article.language
         self.assertEqual(article_language, "en")
 
+    def test_strip_words(self):
+        test_place_name = 'the province county district city'
+        self.assertEqual(strip_words(test_place_name), '')
+        test_place_name = 'the United States'
+        self.assertEqual(strip_words(test_place_name), 'United States')
+
     def test_extract_countries(self):
-        test_article = Article("United Kingdom plus Afghanistan", \
+        test_article = Article("The United Kingdom plus Afghanistan plus Sichuan Province, as well as Toronto, Cuba and Bosnia", \
                                 self.date, "test_title", "test_content_type", [
                                "test_author_1", "test_author_2"], "www.butts.com", "www.butts.com/disasters")
         countries = self.pipeline.extract_countries(test_article)
         self.assertIsInstance(countries, list)
-        self.assertEqual(len(countries), 2)
+        self.assertEqual(len(countries), 6)
         self.assertIn('GB', countries)
         self.assertIn('AF', countries)
+        self.assertIn('CN', countries)
+        self.assertIn('CA', countries)
+        self.assertIn('CU', countries)
+        self.assertIn('BA', countries)
         test_article = Article("No countries mentioned", \
                                 self.date, "test_title", "test_content_type", [
                                "test_author_1", "test_author_2"], "www.butts.com", "www.butts.com/disasters")
