8c4c6a43752b8c842922d1a63169d545e20a6f14
WanderingStar <github@account.loath.org>
2017-03-26 22:14:06 -0500

    added some documentation

--- localdb/docker-entrypoint-initdb.d/id.sh
+++ localdb/docker-entrypoint-initdb.d/id.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
 
+# Set up user and database
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE USER d4d WITH PASSWORD 'democracy';
     CREATE DATABASE id;
     GRANT ALL PRIVILEGES ON DATABASE id TO d4d;
 EOSQL
 
+# Create database schema in that database
 psql -v ON_ERROR_STOP=1 --username "d4d" id < /schema.sql
