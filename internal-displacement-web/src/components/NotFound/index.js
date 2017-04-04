f6869db503925f81bd12f075fc44f8426b8d21ff
wwymak <wwymak@gmail.com>
2017-04-04 09:15:53 +0100

    take out duplicated f/e files that are in the wrong place, more tweaks to docker file

--- internal-displacement-web/src/components/NotFound/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import styles from './styles.css'
-
-const NotFound = () => {
-  return <h1 className={styles.root}>Not Found :(</h1>
-}
-
-export default NotFound
--- internal-displacement-web/src/containers/home.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const Home = () => {
-  return <h2>Home...nothing here yet</h2>
-
-}
-
-export default Home
